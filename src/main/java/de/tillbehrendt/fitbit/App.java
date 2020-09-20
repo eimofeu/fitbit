582382acc95a1bf532fa46d257a46ae1dc32566b
Till Behrendt <till.behrendt@outlook.de>
2020-09-20 14:11:27 +0200

    initial commit

---
+++ src/main/java/de/tillbehrendt/fitbit/App.java
@@ -0,0 +1,13 @@
+package de.tillbehrendt.fitbit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
